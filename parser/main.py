37967c32802e63319edaf31cd445406f189da70f
Domrachev Danil <danildomrachev111@gmail.com>
2025-07-12 16:19:22 +0200

    style: todolist

--- parser/main.py
+++ parser/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/")
+
+#TODO: 
+# Crucial:
+# Bitrix authentication integration
+# Set up nginx public folder
+# Close 8000 port (access to this app should be only using nginx)
+# Define requests limitations
+# Define roles: e.g. only admin can send Purchase Orders, See invoices
+# Define tech stack: nginx, uvicorn, python(version?) fastapi, tailwind, jinja2, axios 
+
+# Architecture
+# Define abstract distributor client, create class for every distributor api
+# Unify distributor filters - if clients share some filters we can compare them, if not - find out how to show them
+
+# Code notes
+# HTML Routing: <a href="/route"> <button> Go there </button> </a>
+
+#Bitrix sends post request 
+#with credentials when app is opened
+@app.post("/")
 def read_root():
     return "<h1>Hello, World!</h1>"
 
